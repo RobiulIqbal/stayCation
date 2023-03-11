9e40fad104886d2d785534117660ea48879d3a51
RobiulIqbal <qobel98@gmail.com>
2023-03-11 22:37:16 +0700

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import "assets/scss/style.scss";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
-  return <div className="App">long life learner</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Route path="/" component={LandingPage} />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- src/elements/Button/index.js
+++ src/elements/Button/index.js
@@ -4,11 +4,11 @@ import propTypes from "prop-types";
 
 export default function Button(props) {
   const className = [props.className];
-  if (props.isPrimary) className.push("btn-primary");
+  if (props.isPrimary) className.push("btn-primary ");
   if (props.isLarge) className.push("btn-lg");
   if (props.isSmall) className.push("btn-sm");
   if (props.isBlock) className.push("btn-block");
-  if (props.hasShadow) className.push("btn-shadow");
+  if (props.hasShadow) className.push("btn-shadow ");
 
   const onClick = () => {
     if (props.onClick) props.onClick();
@@ -32,7 +32,7 @@ export default function Button(props) {
   if (props.type === "link") {
     if (props.isExternal) {
       return (
-        <a href={props.href} className={className.join("")} style={props.style} target={props.target === "_blank" ? "_blank" : undefined} rel={props.target === "_blank" ? "noopener noreferrer" : undefined}>
+        <a href={props.href} className={className.join(" ")} style={props.style} target={props.target === "_blank" ? "_blank" : undefined} rel={props.target === "_blank" ? "noopener noreferrer" : undefined}>
           {props.children}
         </a>
       );
@@ -58,6 +58,7 @@ Button.propTypes = {
   target: propTypes.string,
   className: propTypes.string,
   isDisabled: propTypes.bool,
+  isPrimary: propTypes.bool,
   isExternal: propTypes.bool,
   isLoading: propTypes.bool,
   isSmall: propTypes.bool,
